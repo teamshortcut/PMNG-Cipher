74f2f3475a56a46ad420b6e4105e663cd042bf30
Aaron Cuthertson <3aaron3@gmail.com>
2020-01-26 21:33:18 +0000

    Add functionality for separating numbers

--- cipher.py
+++ cipher.py
@@ -1,17 +1,14 @@
 # Poly-mapping N-gram cipher
 
+import re
 import sys
 from wordfreq import word_frequency
-
-# TODO: handle splitting numbers, if numbers are contained within words,
-# if words are not split by space (maybe split by number, maybe nothing)
-
-# D means double the next character
 
 HELP_MESSAGE = """Usage:
 cipher.py [-h] [-d] text
 -h : displays this help message
 -d : decrypts the given text (default is encrypt)
+-n : separates numbers
 """
 # normal substitution mappings
 
@@ -35,6 +32,7 @@ encrypt_mapping = {"A": "O",
                   }
 decrypt_mapping = {v: k for k, v in encrypt_mapping.items()} # ciphertext to plaintext
 
+# multiple plaintext characters map to the same ciphertext character
 encrypt_polymappings = {"C": "k",
 						"F": "f",
 						"G": "q",
@@ -53,56 +51,65 @@ decrypt_polymappings = {"F": ["f", "th"],
 						"Z": ["q", "x", "z"]
 						}
 
-encrypt_polygrams = {"TH": "f",
-					 "OU": "u",
-					 "EA": "e",
-					 "ING": "i",
-					 "ST": "s"
-					}
-decrypt_polygrams = {v.upper(): k.lower() for k, v in encrypt_polygrams.items()}
-del decrypt_polygrams["F"] # should only be decrypted in polymappings, as could decrypt to F or TH
+# certain n-grams (common groups of characters) encrypt to single ciphertext characters
+encrypt_ngrams = {"TH": "f",
+				  "OU": "u",
+				  "EA": "e",
+				  "ING": "i",
+				  "ST": "s"
+				 }
+# uppercase for unprocessed, lowercase for characters that have been replaced
+decrypt_ngrams = {v.upper(): k.lower() for k, v in encrypt_ngrams.items()}
+del decrypt_ngrams["F"] # should only be decrypted in polymappings, as could decrypt to F or TH
 
+# returns the encrypted equivalent of a plaintext character, if it exists in any mapping
 def tryForMappedCharEncrypt(char):
 	if char in encrypt_mapping.keys():
 		return encrypt_mapping[char]
-	elif char in encrypt_polygrams.keys():
-		return encrypt_polygrams[char]
+	elif char in encrypt_ngrams.keys():
+		return encrypt_ngrams[char]
 	elif char in encrypt_polymappings.keys():
 		return encrypt_polymappings[char]
 	else:
 		return char
 
+# returns the decrypted equivalent of a ciphertext character, if it exists in standard/n-gram mapping
+# does not check for polymappings, this is handled separately
 def tryForMappedCharDecrypt(char):
 	if char in decrypt_mapping.keys():
 		return decrypt_mapping[char].lower()
-	elif char in decrypt_polygrams.keys():
-		return decrypt_polygrams[char].lower()
+	elif char in decrypt_ngrams.keys():
+		return decrypt_ngrams[char].lower()
 	else:
 		return char
 
+# recursive function to get all the combinations of possible decryptions with polymappings
+# expects a partially decrypted ciphertext, the current possibilities (pass [] to start) and the characters to check mappings for
 def getPolymappingPossibilities(ciphertext, possibilities, cipher_chars):
-	i = 0
-	if ciphertext.islower():
+	if ciphertext.islower(): # fully decrypted
 		possibilities.append(ciphertext)
 		return possibilities
 
+	i = 0
 	while i < len(ciphertext):
 		char = ciphertext[i]
 		if char in cipher_chars:
 			local_possibilities = []
-			for c in decrypt_polymappings[char]:
+			for c in decrypt_polymappings[char]: # all possibilities for decrypting this character
 				local_possibilities.append(ciphertext[:i] + c + ciphertext[i+1:])
 			
 			for p in local_possibilities:
+				# all combinations of further characters
 				possibilities = getPolymappingPossibilities(p, possibilities, cipher_chars)
 
 		i += 1
 
-	return list(set(possibilities))
+	return list(set(possibilities)) # unique results
 
 def word_freq(word):
 	return word_frequency(word, "en")
 
+# decrypts the polymappings of a partially decrypted ciphertext
 def decryptPolymapings(ciphertext):
 	plaintext = []
 
@@ -115,9 +122,10 @@ def decryptPolymapings(ciphertext):
 
 		# substitutions to make
 		if cipher_chars:
+			# all possible decryptions based on all combinations of polymappings
 			possibilities = getPolymappingPossibilities(word, [], cipher_chars)
-			possibilities = sorted(possibilities, key=word_freq)
-			plaintext.append(possibilities[-1])
+			possibilities = sorted(possibilities, key=word_freq) # find the closest to english based on the frequency of the word
+			plaintext.append(possibilities[-1]) # last item is most frequent
 		else:
 			plaintext.append(word)
 
@@ -128,12 +136,16 @@ def decrypt(ciphertext, separateNumbers):
 	plaintext = ""
 	numbers = []
 
+	# if the text isn't split by words, split it up by numbers
+	if " " not in ciphertext and any(char.isdigit() for char in ciphertext):
+		ciphertext = " ".join(re.split("(0|1|2|3|4|5|6|7|8|9)", ciphertext))
+
 	index = 0
 	while index < len(ciphertext):
 		char = ciphertext[index]
-		if char == "D":
+		if char == "D": # D means that the following character is duplicated
 			plainChar = tryForMappedCharDecrypt(ciphertext[index+1])
-			if plainChar not in decrypt_polymappings.keys():
+			if plainChar not in decrypt_polymappings.keys(): # leave as uppercase if in polymappings, will need further processing
 				plainChar = plainChar.lower()
 
 			plaintext += plainChar*2
@@ -163,8 +175,8 @@ def encrypt(plaintext):
 	plaintext = plaintext.upper()
 	ciphertext = plaintext
 
-	alphabet = list(encrypt_polygrams.keys()) + list(encrypt_polymappings.keys()) + list(encrypt_mapping.keys())
-
+	# go through mappings, in order, replacing plaintext character with its cipher counterpart
+	alphabet = list(encrypt_ngrams.keys()) + list(encrypt_polymappings.keys()) + list(encrypt_mapping.keys())
 	for char in alphabet:
 		ciphertext = ciphertext.replace(char, tryForMappedCharEncrypt(char).lower())
 
@@ -178,15 +190,15 @@ if __name__ == "__main__":
 		print("At least one argument must be provided.")
 	elif len(sys.argv) > 1 and sys.argv[1] == "-h":
 		print(HELP_MESSAGE)
-	elif len(sys.argv) > 1 and sys.argv[1] == "-d":
+	elif len(sys.argv) > 1 and "-d" in sys.argv:
 		if len(sys.argv) < 3 and not stdinput:
 			print("Please provide ciphertext to decrypt.")
 		else:
 			if stdinput:
 				code = sys.stdin.read()
 			else:
-				code = sys.argv[2]
-			print(decrypt(code, True))
+				code = sys.argv[[i for i, s in enumerate(sys.argv) if '-d' in s][0]+1]
+			print(decrypt(code, "-n" in sys.argv))
 	else:
 		if stdinput:
 			code = sys.stdin.read()
